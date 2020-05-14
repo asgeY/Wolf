066031612ceefd2738977df3bafc93e4c8fc342d
Asgedom <asmow@yahoo.com>
2020-05-13 19:37:22 -0500

    Initial commit

---
+++ Wolf/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Wolves
+//
+//  Created by Asge Yohannes on 5/13/20.
+//  Copyright © 2020 Asge Yohannes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+       Home()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wolf/Home.swift
@@ -0,0 +1,58 @@
+//
+//  Home.swift
+//  Wolves
+//
+//  Created by Asge Yohannes on 5/13/20.
+//  Copyright © 2020 Asge Yohannes. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State var index = 0
+    let images: [String] = ["1", "2", "3", "4", "5", "6"]
+    
+    let imageFrameSize: CGFloat = 50
+    let imageFrameHeight: CGFloat = 700
+    let imageMinScaleRatio: CGFloat = 0.9
+    
+    var body: some View {
+        
+        ZStack {
+            Color("Color").edgesIgnoringSafeArea(.all)
+            VStack(alignment: .center) {
+                Text("Wolves")
+                    .font(.largeTitle)
+                    .padding(.top,5)
+                Spacer()
+                
+            }
+            
+            GeometryReader { geometry in
+                
+                ImageCarousel(index: self.$index.animation(), maxIndex: self.images.count - 1) {
+                    ForEach(0..<self.images.count) { index in
+                        
+                        Image(self.images[index])
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: geometry.size.width - self.imageFrameSize, height: self.imageFrameHeight)
+                            .scaleEffect(self.index == index ? 1.0 : self.imageMinScaleRatio)
+                            .animation(.easeInOut)
+                            .cornerRadius(self.index == index ? 15 : 30)
+                            .clipped()
+                    }
+                }
+            }
+            .frame(height: self.imageFrameHeight + 10)
+          
+        }
+        
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
